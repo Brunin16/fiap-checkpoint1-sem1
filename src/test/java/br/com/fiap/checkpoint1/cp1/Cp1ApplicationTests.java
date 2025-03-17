5fe5eae6ed522c4620661e083b871b4fb9d5b523
brunin16 <bruedu16@gmail.com>
2025-03-17 20:55:37 -0300

    first commit, create project

---
+++ src/test/java/br/com/fiap/checkpoint1/cp1/Cp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.checkpoint1.cp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
